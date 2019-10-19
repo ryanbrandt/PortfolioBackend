8931c6969eb48424d34630f87221f56dda705484
Ryan Brandt <rnb52@scarletmail.rutgers.edu>
2019-10-19 16:58:27 -0400

    cors policy + port update

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ import http from "http";
 import express from "express";
 import logger from "morgan";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import routes from "./server/routes";
 
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 8080;
 const app = express(); // setup express application
 const server = http.createServer(app);
 
+app.use(cors());
 app.use(logger("dev")); // log requests to the console
 
 // Parse incoming requests data
